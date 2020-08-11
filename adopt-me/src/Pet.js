8544f9ee99fb93929a894b43542db3f8eb98c77c
Simran Sandhu <7simransandhu@gmail.com>
2020-08-11 19:33:30 -0400

    completed Hooks
    Added hooks to SearchParams, created custom hook called useDropdown.js to create drop down from API calls for ANimals and Breed

--- adopt-me/src/Pet.js
+++ adopt-me/src/Pet.js
@@ -1,31 +1,13 @@
-// We will have error from ESLint saying we defined but never use React. We do through Babel/JSX but ESlint just does not know that
-
 import React from "react";
-/* REACT: This is the basics of react. It is building components that we can put inside of other components and so on. */
-// Creating another component (or another Stamp)
-// All of our info that is passed to Pet will be sent through the param of props. See how we are accessing props.____ to get the info from our call
-/* ---> or we can do destructuring and inside of writing props.____ everytime we write replace the param props with {____,____,_____} ____ being the properties. The we can use those properties as vars in our code*/
 
-// When exporting full file/constuct from another file, format like this
-export default function Pet({ name, animal, breed }) {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, name),
-  //   React.createElement("h2", {}, animal),
-  //   React.createElement("h2", {}, breed),
-  // ]);
-
-  /* NOTE ABOUT JSX: we could change the file type to jsx to give us jsx features or we can go to the bottom right corner and change into Javascript React */
-
-  /* Also NOTE: You can only return one thing so everything must be wrapped inside of a single element else error */
-
-  /* This does the same as above. Babel translates what is below into React.createElement() calls*/
-
-  /* Inside the {} we can write expressions. An expression is anything that can go on the right side of a var declaration. const x = ___________. Does not have to only be the params. It can also be name.ToUpper() or animal + breed. Stuff like that */
+const Pet = props => {
   return (
     <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
+      <h1>{props.name}</h1>
+      <h2>{props.animal}</h2>
+      <h2>{props.breed}</h2>
     </div>
   );
-}
+};
+
+export default Pet;
--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -1,63 +1,14 @@
 import React from "react";
-//only importing render from react-dom(no longer need ReactDOM.render, now just render) prevents us from importing things that arent going to be used(concept called Tree Shaking. YT it for more info)
-import { render } from "react-dom";
-import Pet from "./Pet";
+import ReactDOM from "react-dom";
+import SearchParams from "./SearchParams";
 
-// *********************************************
-
-// We have created the App component
-/* Think of this as a stamp. We created a stamp called App. This stamp will be used stamped any time we want. So whenever we call App(), it will stamp a div and inside of that it will stamp an h1 */
-/*  NOTE: The CSS is actually having the h1, so its not text its replaced with a logo, the text inside saying "adopt me" therefore is pointless. (Or you can just put them as an Array of upto 15 max children) 
- 
-THis is called variable arity.. (air-ity)
-*/
 const App = () => {
-  // return React.createElement(
-  //   "div",
-  //   {},
-  /*  *************************************************
-            UNDERSTANDING: .createElement
-             >PARAM1: What kind of element we want to create
-             >PARAM2: What attributes we want it to have (id, class, src, etc)
-             >PARAM3: What we want inside of it(childen), i.e: "adopt me" or another createElement
-             >PARAM PAST THAT: All addtional patterns are also considered children
-             *************************************************
-            */
-  // The info we are passing to Pet() is being given as a Param called Props
-  // [
-  //   React.createElement("h1", {}, "Adopt Me"),
-  //   React.createElement(Pet, {
-  //     name: "Luna",
-  //     animal: "Dog",
-  //     breed: "Havanese",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Pepper",
-  //     animal: "Bird",
-  //     breed: "Cockatiel",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Doink",
-  //     animal: "Cat",
-  //     breed: "Mixed",
-  //   }),
-  // ]
-  // );
-
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      {/* How to use other components */}
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };
 
-// Here we are using out stamp of App and rendering it inside the div with #root.
-/* React.createElement can either take in a stamp(component) that you have created or take in a string of an HTML element can create that("h1" would create an h1) */
-/* NOTE IMPORTANT: when we write .render anything inside the element selected will be removed. Hence they "not rendered" is no longer visible */
-
-// React.createElement(App) == <App/>
-render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
