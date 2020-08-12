11ff86d17b99c9b6dd93bb7cb7e83d9702d7af38
Simran Sandhu <7simransandhu@gmail.com>
2020-08-11 22:01:38 -0400

    add Reach Router
    Also added link around "Adopt Me" so on click it will take us to come

--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
 
 const App = () => {
   return (
     // Strictmode will throw errors if we use anything that is/is soon to be deprecated from React. Helps future proof code
     <React.StrictMode>
       <div>
-        <h1>Adopt Me!</h1>
-        <SearchParams />
+        <header>
+          {/* Link allows us to adopt on the logo and go to home page */}
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+        ;
       </div>
     </React.StrictMode>
   );
